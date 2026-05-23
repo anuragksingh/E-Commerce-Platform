e6c3054b32f5e5e0ce2e86e97cffcce0641b2634
Anurag Kumar Singh <anuragksingh.dev@gmail.com>
2026-05-23 16:52:35 +0530

    Fix CSS import casing in ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../pageStyles/productDetails.css";
+import "../pageStyles/ProductDetails.css";
 import PageTitle from "../components/PageTitle";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
